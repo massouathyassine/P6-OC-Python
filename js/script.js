42b9a2d879b746aae37b063a6c0b8d83797b4d28
massouathyassine <64510318+massouathyassine@users.noreply.github.com>
2021-12-26 15:01:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,159 @@
+getDataByCat('comedy')
+getDataByCat('horror')
+getDataByCat('sci-fi')
+getDataByBest()
+slider("1")
+slider("2")
+slider("3")
+slider("4");
+
+
+function slider(n) {
+  let slider = document.querySelector(".slider"+n),
+    last = slider.lastElementChild,
+    first = slider.firstElementChild,
+    btn = document.querySelectorAll(".btn"+n);
+
+  slider.insertBefore(last, first);
+
+  btn.forEach(btn => {
+    btn.addEventListener("click", movement);
+  });
+
+  function movement(e) {
+    slider = document.querySelector(".slider"+n);
+    last = slider.lastElementChild;
+    first = slider.firstElementChild;
+
+    const activeSlide = document.querySelector(".active");
+
+    if (e.target.id === "next") {
+      slider.insertBefore(first, last.nextSibling);
+
+      activeSlide.classList.remove("active");
+      activeSlide.nextElementSibling.classList.add("active");
+    } else {
+      slider.insertBefore(last, first);
+      activeSlide.classList.remove("active");
+      activeSlide.previousElementSibling.classList.add("active");
+    }
+  }
+}
+
+
+function getDataByBest() {
+    let resultsp1;
+    let resultsp2;
+    let results;
+    let url_page1 = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score';
+    let url_page2 = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page=2';
+
+    $.get(url_page1).done(
+        function(data) {
+            resultsp1 = data.results;
+            $.get(url_page2).done(
+                function(data) {
+                    resultsp2 = data.results;
+
+                    results = resultsp1.concat(resultsp2);
+
+                    ContentBestMovie(results)
+                }
+            );
+        }
+    );
+
+}
+
+function ContentBestMovie(data) {
+
+    let best = document.getElementById('best-movie');
+    let button = document.getElementById('best-button');
+    let bestdesc = document.getElementById('best-desc');
+    let besttitle = document.getElementById('best-title');
+
+    //meuilleur film imdb
+    let bestimg = best.getElementsByTagName('img')[0];
+    bestimg.src = data[0].image_url;
+    bestimg.alt = data[0].title;
+    button.setAttribute('onclick','showModal(' + data[0].id + ')');
+    besttitle.innerHTML = data[0].title;
+
+    $.get('http://localhost:8000/api/v1/titles/' + data[0].id).done(
+        function(data) {
+            bestdesc.innerHTML = data.long_description
+        });
+
+
+
+    let slider = document.getElementById('best');
+    let sliders = slider.getElementsByClassName('movie_item');
+
+    for (let i = 0; i < sliders.length; i++) {
+
+        let img = sliders[i].getElementsByTagName('img')[0];
+        img.src = data[i+1].image_url;
+        img.alt = data[i+1].title;
+        img.setAttribute('onclick','showModal(' + data[i+1].id +')' )
+
+      };
+
+
+
+};
+
+function getDataByCat(genre) {
+
+    let resultsp1;
+    let resultsp2;
+    let results;
+    let url_page1 = 'http://localhost:8000/api/v1/titles/?genre=' + genre + '&sort_by=-imdb_score';
+    let url_page2 = 'http://localhost:8000/api/v1/titles/?genre=' + genre + '&sort_by=-imdb_score&page=2';
+
+    $.get(url_page1).done(
+        function(data) {
+            resultsp1 = data.results;
+
+            $.get(url_page2).done(
+                function(data) {
+                    resultsp2 = data.results;
+
+                    results = resultsp1.concat(resultsp2);
+
+                    SliderContent(genre, results)
+                }
+            );
+        }
+    );
+};
+
+
+function SliderContent(genre, data) {
+
+    let slider = document.getElementById(genre);
+    let sliders = slider.getElementsByClassName('movie_item');
+
+
+    for (let i = 0; i < sliders.length; i++) {
+
+        let img = sliders[i].getElementsByTagName('img')[0];
+        img.src = data[i].image_url;
+        img.alt = data[i].title;
+        img.setAttribute('onclick','showModal(' + data[i].id +')' )
+
+      };
+
+};
+
+
+
+
 
 let modal = document.getElementById("modalBox");
 let span = document.getElementsByClassName("close")[0];
 let body = document.getElementsByTagName('body')[0]
 
-let changeModalContent = function(data) {
+let ModalContent = function(data) {
     //récupérer les informations d'une balise identifiée par son id
     let title = document.getElementById('movie-title');
     let desc = document.getElementById('desc');
@@ -29,7 +179,7 @@ let changeModalContent = function(data) {
 function showModal(movieid) {
 
     let url = "http://localhost:8000/api/v1/titles/" + movieid;
-    $.get(url).done(changeModalContent);
+    $.get(url).done(ModalContent);
     modal.style.display = "block";
 
     document.getElementsByClassName('container')[0].style.filter = 'blur(10px)';
@@ -53,170 +203,6 @@ window.onclick = function (event) {
 };
 
 
-function getDataByBest() {
-    let resultsp1;
-    let resultsp2;
-    let results;
-    let url_page1 = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score';
-    let url_page2 = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page=2';
 
-    $.get(url_page1).done(
-        function(data) {
-            resultsp1 = data.results;
-
-            $.get(url_page2).done(
-                function(data) {
-                    resultsp2 = data.results;
-
-                    results = resultsp1.concat(resultsp2);
-
-                    changeContentForBest(results)
-                }
-            );
-        }
-    );
-
-}
-
-
-function changeContentForBest(data) {
-
-    let best = document.getElementById('best-movie');
-    let button = document.getElementById('best-button');
-    let bestdesc = document.getElementById('best-desc');
-    let besttitle = document.getElementById('best-title')
-
-    let bestimg = best.getElementsByTagName('img')[0];
-    bestimg.src = data[0].image_url;
-    bestimg.alt = data[0].title;
-    button.setAttribute('onclick','showModal(' + data[0].id + ')');
-    besttitle.innerHTML = data[0].title
-
-
-    $.get('http://localhost:8000/api/v1/titles/' + data[0].id).done(
-        function(data) {
-            bestdesc.innerHTML = data.long_description
-        })
-
-
-
-    let slider = document.getElementById('best');
-    let sliders = slider.getElementsByClassName('movie_item');
-
-    for (let i = 0; i < sliders.length; i++) {
-
-        let img = sliders[i].getElementsByTagName('img')[0];
-        img.src = data[i+1].image_url;
-        img.alt = data[i+1].title;
-        img.setAttribute('onclick','showModal(' + data[i+1].id +')' )
-
-      };
-
-
-
-};
-
-
-
-function getDataByCat(genre) {
-
-    let resultsp1;
-    let resultsp2;
-    let results;
-    let url_page1 = 'http://localhost:8000/api/v1/titles/?genre=' + genre + '&sort_by=-imdb_score';
-    let url_page2 = 'http://localhost:8000/api/v1/titles/?genre=' + genre + '&sort_by=-imdb_score&page=2';
-
-    $.get(url_page1).done(
-        function(data) {
-            resultsp1 = data.results;
-
-            $.get(url_page2).done(
-                function(data) {
-                    resultsp2 = data.results;
-
-                    results = resultsp1.concat(resultsp2);
-
-                    changeSliderContent(genre, results)
-                }
-            );
-        }
-    );
-};
-
-
-function changeSliderContent(genre, data) {
-
-    let slider = document.getElementById(genre);
-    let sliders = slider.getElementsByClassName('movie_item');
-
-
-    for (let i = 0; i < sliders.length; i++) {
-
-        let img = sliders[i].getElementsByTagName('img')[0];
-        img.src = data[i].image_url;
-        img.alt = data[i].title;
-        img.setAttribute('onclick','showModal(' + data[i].id +')' )
-
-      };
-
-};
-
-
-
-
-
-
-function moveLeft(sliderid) {
-    let slider, slidercontent, textIndent;
-
-    slider = document.getElementById(sliderid);
-
-    if (!slider) return false;
-
-    slidercontent = slider.getElementsByClassName('slidercontent')[0];
-
-    textIndent = parseInt(slider.style.textIndent || 0);
-
-    slider.style.textIndent=(textIndent-25)+"%";
-
-    slider.getElementsByClassName('arrow left')[0].style.visibility = "visible";
-
-
-    if (slider.style.textIndent >= -50+"%")  {
-       slider.style.textIndent= -50+"%";
-       slider.getElementsByClassName('arrow right')[0].style.visibility = "hidden";
-    };
-
-    return true;
-};
-
-function moveRight(sliderid) {
-    let slider, slidercontent, textIndent;
-
-    slider = document.getElementById(sliderid);
-
-    if (!slider) return false;
-
-    slidercontent = slider.getElementsByClassName('slidercontent')[0];
-
-    textIndent = parseInt(slider.style.textIndent || 0);
-
-    slider.style.textIndent=(textIndent+25)+"%";
-
-    slider.getElementsByClassName('arrow right')[0].style.visibility = "visible";
-
-    if (slider.style.textIndent >= 0+"%")  {
-        slider.style.textIndent=(0)+"%";
-        slider.getElementsByClassName('arrow left')[0].style.visibility = "hidden";
-    };
-
-    return true;
-};
-
-setTimeout(function(){ document.getElementById('loading').style.display = 'none'; }, 1000);
-getDataByCat('comedy')
-getDataByCat('horror')
-getDataByCat('sci-fi')
-getDataByBest()
 
 
